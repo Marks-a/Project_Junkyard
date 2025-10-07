d09f24f9bed939e2f72834a8322c9f25f937f1b8
Marks <marks.auzins@va.lv>
2025-10-07 10:52:51 +0000

    Working crossplatform Windows (Needs 2 run.sh for it complete run, cuz some links contain PS that linux does not have)

--- C-based/CPP_Rendering_Engine/run.sh
+++ C-based/CPP_Rendering_Engine/run.sh
@@ -8,4 +8,25 @@ cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=/home/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 cmake --build .
 
-./TestSDL2GLM
+#./TestSDL2GLM
+echo "Linux part output complete"
+
+
+# Windows out
+mkdir -p ./out
+
+cmake -B build \
+      -S .. \
+      -DCMAKE_TOOLCHAIN_FILE=/home/vcpkg/scripts/buildsystems/vcpkg.cmake \
+      -DVCPKG_TARGET_TRIPLET=x64-mingw-static \
+      -DCMAKE_SYSTEM_NAME=Windows \
+      -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+      -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+      -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build --config Release
+
+cp build/TestSDL2GLM.exe ./out/TestSDL2GLM.exe
+echo 'Windows exe is created'
+
+echo "Windows part output complete"
