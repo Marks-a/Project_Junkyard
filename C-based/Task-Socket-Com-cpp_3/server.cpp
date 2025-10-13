73a0bc0f04b121d434075b7dd226c404fce0cecf
Marks <marks.auzins@va.lv>
2025-10-13 11:30:21 +0000

    Added the basic folder and the basic structure for server.cpp

---
+++ C-based/Task-Socket-Com-cpp_3/server.cpp
@@ -0,0 +1,18 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct localMachiene {
+    std::string name;
+    std::string ip;
+    std::string subnet;
+    std::string mac;
+    int port;
+};
+
+
+int main() {
+    printf("Server is running...\n");
+    return 0;
+}
