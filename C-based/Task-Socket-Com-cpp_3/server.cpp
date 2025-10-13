d2a1d2d3cbe082532a6acb7d0a0ba6d9f300bba5
Marks <marks.auzins@va.lv>
2025-10-13 12:53:29 +0000

    Added a get mac address function (May need to optimize)

--- C-based/Task-Socket-Com-cpp_3/server.cpp
+++ C-based/Task-Socket-Com-cpp_3/server.cpp
@@ -8,6 +8,10 @@
 #include <arpa/inet.h> 
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <unistd.h>
+
+void get_local_machine_info();
+void get_mac_address(const std::string &interface_name);
 
 struct localMachieneInfo {
     std::string name;
@@ -28,11 +32,29 @@ void get_local_machine_info() {
             char ip[INET_ADDRSTRLEN]; 
             inet_ntop(AF_INET, &((struct sockaddr_in *)iface->ifa_addr)->sin_addr, ip, sizeof(ip)); 
             std::cout << "Interface: " << iface->ifa_name << " IP Address: " << ip << std::endl;
-
+            get_mac_address(iface->ifa_name);
         } 
     } 
     freeifaddrs(interfaces); 
 };
+void get_mac_address(const std::string &interface_name) {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1) {
+        perror("socket");
+        return;
+    }
+    struct ifreq ifr;
+    strncpy(ifr.ifr_name, interface_name.c_str(), IFNAMSIZ-1);
+    if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
+        perror("ioctl");
+        close(fd);
+        return;
+    }
+    close(fd);
+    unsigned char *mac = reinterpret_cast<unsigned char *>(ifr.ifr_hwaddr.sa_data);
+    printf("MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    }
 
 int main() {
     printf("Getting info...\n");
