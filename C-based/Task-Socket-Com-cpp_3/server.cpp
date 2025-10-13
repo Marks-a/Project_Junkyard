81a371d2b28e5bf7859b1d57765800cd632a2988
Marks <marks.auzins@va.lv>
2025-10-13 13:25:43 +0000

    Local machine info gather works (Tested on 1 connected eth, without loop)

--- C-based/Task-Socket-Com-cpp_3/server.cpp
+++ C-based/Task-Socket-Com-cpp_3/server.cpp
@@ -10,17 +10,18 @@
 #include <net/if.h>
 #include <unistd.h>
 
-void get_local_machine_info();
-void get_mac_address(const std::string &interface_name);
-
-struct localMachieneInfo {
+struct localMachineInfo {
     std::string name;
     std::string ip;
     std::string subnet;
     std::string mac;
     int port;
 };
-void get_local_machine_info() {
+
+void get_local_machine_info(std::vector<localMachineInfo> &infoList);
+void get_mac_address(const std::string &interface_name, std::string &mac_address);
+
+void get_local_machine_info(std::vector<localMachineInfo> &infoList) {
     struct ifaddrs *interfaces = nullptr; 
     if (getifaddrs(&interfaces) == -1) {
         perror("getifaddrs");
@@ -32,12 +33,16 @@ void get_local_machine_info() {
             char ip[INET_ADDRSTRLEN]; 
             inet_ntop(AF_INET, &((struct sockaddr_in *)iface->ifa_addr)->sin_addr, ip, sizeof(ip)); 
             std::cout << "Interface: " << iface->ifa_name << " IP Address: " << ip << std::endl;
-            get_mac_address(iface->ifa_name);
+            localMachineInfo info;       
+            info.name = iface->ifa_name;
+            info.ip = ip;
+            get_mac_address(iface->ifa_name, info.mac);
+            infoList.push_back(info);
         } 
     } 
     freeifaddrs(interfaces); 
 };
-void get_mac_address(const std::string &interface_name) {
+void get_mac_address(const std::string &interface_name , std::string &mac_address) {
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (fd == -1) {
         perror("socket");
@@ -54,11 +59,21 @@ void get_mac_address(const std::string &interface_name) {
     unsigned char *mac = reinterpret_cast<unsigned char *>(ifr.ifr_hwaddr.sa_data);
     printf("MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
            mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+        char mac_str[18];
+        snprintf(mac_str, sizeof(mac_str), "%02x:%02x:%02x:%02x:%02x:%02x",
+                 mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+        mac_address = mac_str;
     }
 
 int main() {
     printf("Getting info...\n");
-    get_local_machine_info();
+    std::vector<localMachineInfo> infoList;
+    get_local_machine_info(infoList);
+
+    printf("Info gathered:\n");
+    for (const auto &info : infoList) {
+        std::cout << "Interface: " << info.name << ", IP: " << info.ip << ", MAC: " << info.mac << std::endl;
+    }
 
     return 0;
 }
